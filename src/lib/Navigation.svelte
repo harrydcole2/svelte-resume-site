48900a867fd4c137b90e0fb105ce62a9b7cb4f12
Harrison Cole <harrydcole2@gmail.com>
2024-07-07 21:12:13 -0600

    Navigation with some more content on expand

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -30,31 +30,38 @@
     scrolled={isScrolled}
     style="transition: all 0.3s ease; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);"
   >
-    <NavbarBrand href="/">
-      <img
-        src="/src/assets/HarrisonPhoto.jpg"
-        alt="Avatar"
-        class:avatar-shrunk={isScrolled}
-        class="avatar"
-      />
-    </NavbarBrand>
-    <Nav class="ml-auto" navbar pills>
-      <NavItem>
-        <NavLink href="/">Home</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="/about">About</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="#contact">Contact</NavLink>
-      </NavItem>
-    </Nav>
+    <div class="navbar-content navbar-brand">
+      <NavbarBrand href="/">
+        <img
+          src="/src/assets/HarrisonPhoto.jpg"
+          alt="Avatar"
+          class:avatar-shrunk={isScrolled}
+          class="avatar"
+        />
+        {#if !isScrolled}
+          <div class="page-title">Harrison Cole</div>
+        {/if}
+      </NavbarBrand>
+      <div class:align-bottom={!isScrolled}>
+        <Nav navbar pills>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/about">About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#contact">Contact</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    </div>
   </Navbar>
 </div>
 
 <style>
   :root {
-    --navbar-bg: #5dd9c1;
+    --navbar-bg: #76d3c0;
     --navbar-bg-scrolled: #c4ffea;
   }
 
@@ -76,5 +83,27 @@
 
   .not-scrolled {
     background-color: var(--navbar-bg) !important;
+  }
+
+  .navbar-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 100%;
+  }
+
+  .navbar-brand {
+    display: flex;
+    align-items: center;
+  }
+
+  .page-title {
+    font-weight: bold;
+    margin-left: 1rem;
+  }
+
+  .align-bottom {
+    align-self: flex-end;
   }
 </style>
